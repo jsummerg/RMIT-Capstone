8fdfe0d93e873cc894c9fa473eca3ec985d393e1
James Summergreene <james.summergreene@gmail.com>
2022-07-27 14:21:48 +1000

    code tidy-up

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,7 +7,7 @@ const wethbitApi = process.env.WEATHERBIT_API
 const pixBaseURL = "https://pixabay.com/api/?key="
 const pixApi = process.env.PIXABAY_API
 
-// http://api.geonames.org/postalCodeSearchJSON?placename=Melbourne&maxRows=5&username=demo example geoname api call
+// http://api.geonames.org/SearchJSON?placename=Melbourne&maxRows=5&username=demo example geoname api call
 // https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=API_KEY example current weatherbit api call
 // https://api.weatherbit.io/v2.0/forecast/daily?lat=35.7796&lon=-78.6382&key=API_KEY example forecast weatherbit api call
 // https://pixabay.com/api/?key=API_KEY&q=Melbourne&category=travel&orientation=horizontal&per_page=5 example pixabay api call
@@ -27,7 +27,6 @@ async function handleSubmit(e){
 
     getGeoName(geoBaseURL, dest, geoUser) // Get geo name location
     .then(function(geoData){
-        console.log(geoData.geonames[0].countryCode) // TO-DO Remove
         if (daysTill < 7) { // If arrival date is less than 16 days
             getCurrentWeather(curWethbitBaseURL, geoData.geonames[0].lat, geoData.geonames[0].lng, wethbitApi)
             .then(function(curData){
@@ -43,7 +42,7 @@ async function handleSubmit(e){
                 console.log(forData.data[daysTill - 1].valid_date)
             })
         }
-        console.log(pixApi)
+
         getDestImg(pixBaseURL, geoData.geonames[0].name, pixApi)
         .then(function(imgData){
             console.log(imgData.hits[0].webformatURL)
@@ -59,7 +58,7 @@ async function handleSubmit(e){
 //Get geo name data
 const getGeoName = async (geoBaseURL, dest, geoUser) =>{
     const res = await fetch(`${geoBaseURL}name=${dest}&maxRows=5&username=${geoUser}`)
-    try { //API call using the baseurl, zipcode and api key
+    try { // API call data
         const data = await res.json()
         console.log(data) // TO-DO Remove
         return data
@@ -71,7 +70,7 @@ const getGeoName = async (geoBaseURL, dest, geoUser) =>{
 //Get Current Weather
 const getCurrentWeather = async (forWethbitBaseURL, lat, lon, wethbitApi) =>{
     const res = await fetch(`${forWethbitBaseURL}lat=${lat}&lon=${lon}&key=${wethbitApi}`)
-    try { //API call using the baseurl, zipcode and api key
+    try { // API call data
         const data = await res.json()
         console.log(data) // TO-DO Remove
         return data
@@ -83,7 +82,7 @@ const getCurrentWeather = async (forWethbitBaseURL, lat, lon, wethbitApi) =>{
 //Get Forecast Weather
 const getForecastWeather = async (curWethbitBaseURL, lat, lon, wethbitApi) =>{
     const res = await fetch(`${curWethbitBaseURL}lat=${lat}&lon=${lon}&key=${wethbitApi}`)
-    try { //API call using the baseurl, zipcode and api key
+    try { // API call data
         const data = await res.json()
         console.log(data) // TO-DO Remove
         return data
@@ -93,9 +92,9 @@ const getForecastWeather = async (curWethbitBaseURL, lat, lon, wethbitApi) =>{
 }
 
 //Get image of destination 
-const getDestImg = async (pixBaseURL, dest, pixApi) =>{ // https://pixabay.com/api/?key=API_KEY&q=yellow+flowers&orientation=horizontal&per_page=5
+const getDestImg = async (pixBaseURL, dest, pixApi) =>{
     const res = await fetch(`${pixBaseURL}${pixApi}&q=${encodeURIComponent(dest)}&orientation=horizontal&category=travel&per_page=5`)
-    try { //API call using the baseurl, zipcode and api key
+    try { // API call data
         const data = await res.json()
         console.log(data) // TO-DO Remove
         return data
